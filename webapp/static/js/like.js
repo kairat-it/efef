2e0c7ecf73c349e71214f14fecb45564a55ccdd3
Kairat <ischenbaew@gmail.com>
2024-08-28 23:58:53 +0200

    создал js

---
+++ webapp/static/js/like.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.like-btn').forEach(button => {
+        button.addEventListener('click', function () {
+            const postId = this.dataset.postId;
+            const action = this.dataset.action;
+            const url = `/api/posts/${postId}/${action}/`;
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken'),
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'post liked') {
+                    this.innerText = 'Unlike';
+                    this.dataset.action = 'unlike';
+                } else if (data.status === 'post unliked') {
+                    this.innerText = 'Like';
+                    this.dataset.action = 'like';
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    });
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
