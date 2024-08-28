e9151674d904637525ed1315069456f424d543ba
Kairat <ischenbaew@gmail.com>
2024-08-28 23:55:46 +0200

    В api/urls.py настроил маршруты для API

--- api_v1/views.py
+++ api_v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 from webapp.models import Post
+from .permissions import IsAuthor
 from .serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
